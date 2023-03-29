5b8b752cee30f7b0426d3fa1761b86fc6aa5e720
Raghav <kumarraghav71950@gmail.com>
2023-03-29 17:21:22 +0530

    first commit

--- src/counterContext.js
+++ src/counterContext.js
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 
 
@@ -8,6 +8,7 @@ const counterContext = createContext()
 
 
 const CounterStateContext = (props) => {
+    const[count,setCount] = useState(0);
 
     return (
         <div id="counter-context">
